0f3dfbe98d414ad4c56ec8fb38cf678a8c589ee3
monika <monikamahanthappa@gmail.com>
2023-10-17 11:11:47 +0530

    Add. Event name presence validation

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    it 'should fail when the name of the event is empty' do
+      event_without_name = build(:event, name: '')
+      expect(event_without_name).to_not be_valid
+    end
+
+    it 'should pass when the name of the event is present' do
+      event_with = build(:event, name: 'A')
+      expect(event_with).to be_valid
+    end
+  end
 end
